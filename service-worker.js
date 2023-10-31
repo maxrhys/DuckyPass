06dac13fd72f0f214847d2e124bae1e1053ef637
maxrhys <66040395+maxrhys@users.noreply.github.com>
2023-10-31 13:05:58 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+// Service Worker installation
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/duckylooks.css',
+        '/favicon.png',
+        '/icon.png',
+        '/duckypass_logo.png',
+        '/duckygen.js',
+        '/service-worker.js'
+        '/manifest.json'
+        // Add other resources that you want to cache for offline use
+      ]);
+    })
+  );
+});
+
+// Service Worker activation and cache cleanup
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== 'my-pwa-cache';
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Service Worker fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
