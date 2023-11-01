462576fb823d9e05ef2886b689624a92fd3ef0ea
maxrhys <66040395+maxrhys@users.noreply.github.com>
2023-11-01 12:35:19 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,46 @@
-// Service Worker installation
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-pwa-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/duckylooks.css',
-        '/favicon.png',
-        '/duckypass_logo.png',
-        '/duckygen.js',
-        '/service-worker.js', // Add a comma here
-        '/service.js'          // Add a comma here
-        // Add other resources that you want to cache for offline use
-      ]);
-    })
-  );
-});
 
-// Service Worker activation and cache cleanup
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== 'my-pwa-cache';
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+            importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+            
+            const routing = workbox.routing;
+            const strategies = workbox.strategies;
+            
+            workbox.routing.registerRoute(
+            	/.(?:css|js|jsx|json)(?|$)/,
+            	new workbox.strategies.StaleWhileRevalidate({
+            		"cacheName": "assets",
+            		plugins: [
+            			new workbox.expiration.Plugin({
+            				maxEntries: 1000,
+            				maxAgeSeconds: 31536000
+            			})
+            		]
+            	})
+            );
+            
+            workbox.routing.registerRoute(
+            	/.(?:png|jpg|jpeg|gif|woff2)$/,
+            	new workbox.strategies.CacheFirst({
+            		"cacheName": "images",
+            		plugins: [
+            			new workbox.expiration.Plugin({
+            				maxEntries: 1000,
+            				maxAgeSeconds: 31536000
+            			})
+            		]
+            	})
+            );
+            
+            workbox.routing.registerRoute(
+            	/(\/)$/,
+            	new workbox.strategies.StaleWhileRevalidate({
+            		"cacheName": "startPage",
+            		plugins: [
+            			new workbox.expiration.Plugin({
+            				maxEntries: 1000,
+            				maxAgeSeconds: 31536000
+            			})
+            		]
+            	})
+            );
 
-// Service Worker fetch event
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Check if the PWA is not already installed
-if (window.matchMedia('(display-mode: browser)').matches && !window.navigator.standalone) {
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the default browser installation prompt
-    e.preventDefault();
-    // Show the browser's installation prompt
-    e.prompt();
-  });
-}
+        
